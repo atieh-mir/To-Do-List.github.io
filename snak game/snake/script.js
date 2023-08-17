3eb0733b611ad3a1041d2ad34e1d22b8a155d86f
atieh_mir <atiehmir17@gmail.com>
2023-08-17 17:29:36 +0330

    Snake game

--- snak game/snake/script.js
+++ snak game/snake/script.js
@@ -1,68 +1,59 @@
-const playBoard=document.querySelector(".play-board");
+const playBoard = document.querySelector(".play-board");
 
-let createSnake,createFood;
-let foodX,foodY;
-let snakeX=20,snakeY=5;
-let snakeBody=[];
-let velocityX=0,velocityY=0;
+let createSnake, createFood;
+let foodX, foodY;
+let snakeX = 20, snakeY = 5;
+let snakeBody = [];
+let velocityX = 0, velocityY = 0;
 
-
-function callFunc(){
+function callFunc() {
     changeFoodPosition();
 }
-const changeFoodPosition=()=>{
-    foodX=Math.floor(Math.random()*30)+1;
-    foodY=Math.floor(Math.random()*30)+1;
+
+const changeFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
 }
- const createElement=()=>{
 
-     createFood=`<div class="food" style="background:red;grid-area:${foodY}/${foodX}"</div>`
-     playBoard.innerHTML=createFood;
+const createElement = () => {
+    createFood = `<div class="food" style="background:red;grid-area:${foodY}/${foodX};"></div>`;
 
-
-     if (snakeX===foodX&&snakeY===foodY){
-         changeFoodPosition();
-        snakeBody.push([foodX,foodY]);
-
-     }
-     for (let i=snakeBody.length;i>0;i--){
-         snakeBody[i]=snakeBody[i-1]
-     }
-    snakeBody[0]=[snakeX,snakeY];
-     snakeX+=velocityX;
-     snakeY+=velocityY;
-     for (let i=0;i<snakeBody.length;i++) {
-     createSnake=`<div class="snake" style="background:green;grid-area:${snakeBody[i][1]}/${snakeBody[i][0]}"</div>`
+    if (snakeX === foodX && snakeY === foodY) {
+        changeFoodPosition();
+        snakeBody.push([foodX, foodY]);
     }
-     playBoard.innerHTML+=createSnake;
- }
 
-   document.addEventListener("keydown",   moveSnake=(e)=>{
-       if (e.keyCode === 38) {
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = [...snakeBody[i - 1]];
+    }
+    snakeBody[0] = [snakeX, snakeY];
+    snakeX += velocityX;
+    snakeY += velocityY;
 
-           velocityX=0;
-           velocityY=-1;
-       }
-        else if (e.keyCode === 40) {
+    let snakeElements = '';
+    for (let i = 0; i < snakeBody.length; i++) {
+        snakeElements += `<div class="snake" style="background:green;grid-area:${snakeBody[i][1]}/${snakeBody[i][0]};"></div>`;
+    }
 
-           velocityX=0;
-           velocityY=1;
-       }
-        else if (e.keyCode === 37) {
-           velocityX=-1;
-           velocityY=0;
-       } else if (e.keyCode === 39) {
-           velocityX=1;
-           velocityY=0;
-       }
+    playBoard.innerHTML = createFood + snakeElements;
+}
 
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.key === "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    }
+});
 
-   });
+setInterval(createElement, 125);
+callFunc();
 
-
-
-
-
-setInterval( createElement,125);
-
- callFunc();
